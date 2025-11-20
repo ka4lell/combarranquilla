4bff68014c474073ed35671a014bf5df7e35a3c5
ka4lel <kalel.dvp@gmail.com>
2025-11-20 13:51:35 -0500

    .

--- sistema_gestionvoluntariado_ongs/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "clave-super-segura-kalel"
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root@localhost/proyectofinal"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
